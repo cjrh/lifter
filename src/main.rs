38777920af95e841c6568c0db90cc280096596a0
Caleb Hattingh <caleb.hattingh@gmail.com>
2021-06-14 21:04:41 +1000

    Added better comments

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,28 @@ fn main(args: Args) -> Result<()> {
     let current_dir = std::env::current_dir()?;
     let output_dir = args.output_dir.or(Some(current_dir)).unwrap();
 
-    // Partition config sections to separate templates out
+    // One of the sections in the .ini file could be a group of
+    // templates. A template is a collection of fields with
+    // default values. A "real" (non-template) section can 
+    // refer to a template by name. When this happens, the 
+    // fields in that template will get substituted into 
+    // that section's values.
+    //
+    // Before we do anything, collect all the template sections
+    // and separate them out from the "real" sections
+
+    // This will hold the templates. The key is the name
+    // of the template and the value is another hashmap of
+    // each of the fields and field values within that template.
     let mut templates = HashMap::new();
+    // This will hold the "real" sections
     let mut sections = vec![];
     sections_raw.into_iter()
         .for_each(|(name, seciter)| {
             if name.starts_with("template:") {
+                // This inner map (inside a particular template)
+                // will store each of the fields and values
+                // for that template.
                 let mut inner_map = HashMap::new();
                 seciter.for_each(|(field, value)| {
                     inner_map.insert(field.clone(), value.clone());
@@ -53,10 +69,12 @@ fn main(args: Args) -> Result<()> {
                     inner_map,
                 );
             } else {
+                // This is not a template so move it into 
+                // the "real" sections list.
                 sections.push((name.clone(), seciter));
             }
         });
-    println!("found templates: {:?}", templates);
+    trace!("Detected templates: {:?}", templates);
 
     sections.par_iter().for_each(|(section, _hm)| {
         match lifter::run_section(section, &conf, filename, &output_dir) {
